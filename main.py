52891c41de166244916f9421b629cb59f4771d58
Michail <Osminogr@yandex.ru>
2022-05-26 13:35:13 +0700

    first-commit

---
+++ main.py
@@ -0,0 +1,52 @@
+
+from math import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+xmin = 0
+xmax = 35
+resolution = 3000
+plt.ylim([-5,5])
+plt.xlim([xmin,xmax])
+
+def integr_family(func,mx=0,my_range = np.linspace(-5,5,10)):
+    for my in my_range:
+        X = np.linspace(xmin,xmax,resolution)
+        X = np.insert(X,0,mx)
+        X.sort()
+
+        index_of_mx= np.where(X == mx)[0][0]
+
+        Y_right = []
+        Y_left = []
+        dx = (xmax-xmin)/resolution
+        #Расчитываем положительный ход
+        y = my
+        for x in X[index_of_mx:-1]:
+            kn = func(x,y)
+            dy = kn*dx
+            yn = y + dy
+            Y_right.append(yn)
+            y = yn
+        #Расчитываем отрицательный ход
+        y = my
+        for x in X[index_of_mx:0:-1]:
+            kn = func(x,y)
+            dy = kn*dx
+            yn = y + dy
+            Y_left.append(yn)
+            y = yn
+
+        Y_left.reverse()
+        #Склеиваем
+        Y = Y_left+[my]+Y_right
+        plt.plot(X,Y,'-b',mx,my,'ro')
+    plt.grid()
+    plt.show()
+    
+
+
+integr_family(lambda x,y : atan(x)/x**2,mx=3.5)
+
+
