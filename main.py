4f823c95a98eeca07a6ec8d472375b65e79ced43
Michail <Osminogr@yandex.ru>
2022-05-27 13:46:03 +0700

    Переписал X

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 
+from fileinput import filename
 from math import *
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-xmin = 0
-xmax = 35
-resolution = 3000
+xmin = -2
+xmax =  3
+step = 0.001
+plt.figure(figsize=(10,15))
 plt.ylim([-5,5])
 plt.xlim([xmin,xmax])
 
 def integr_family(func,mx=0,my_range = np.linspace(-5,5,10)):
     for my in my_range:
-        X = np.linspace(xmin,xmax,resolution)
-        X = np.insert(X,0,mx)
+        X = [mx]
+        i = mx - step
+        while min(X) > xmin:
+            X.append(i)
+            i-=step
+        i = mx + step
+        while max(X) < xmax:
+            X.append(i)
+            i+=step
+        X = np.array(X)
         X.sort()
-
         index_of_mx= np.where(X == mx)[0][0]
 
         Y_right = []
         Y_left = []
-        dx = (xmax-xmin)/resolution
+        dx = step
         #Расчитываем положительный ход
         y = my
         for x in X[index_of_mx:-1]:
@@ -34,7 +43,7 @@ def integr_family(func,mx=0,my_range = np.linspace(-5,5,10)):
         for x in X[index_of_mx:0:-1]:
             kn = func(x,y)
             dy = kn*dx
-            yn = y + dy
+            yn = y - dy
             Y_left.append(yn)
             y = yn
 
@@ -45,8 +54,8 @@ def integr_family(func,mx=0,my_range = np.linspace(-5,5,10)):
     plt.grid()
     plt.show()
     
+    
+    
 
 
-integr_family(lambda x,y : atan(x)/x**2,mx=3.5)
-
-
+integr_family(lambda x,y : (y**2-x**2)/(2*x*y) ,mx=2,my_range=np.linspace(1,5,10))
